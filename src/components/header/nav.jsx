b34e21121b57f21db74683d42bfcc3f4baf65cd4
Bright Udoette <brightudoette@gmail.com>
2024-07-10 12:46:18 +0100

    Modify the navigation content to be navigable with keyboard and ensure accessibility

--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -7,22 +7,29 @@ import handleToggle from "./menuToggle";
 const Nav = () => {
   const [isNavVisible, setIsNavVisible] = useState(false);
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      handleToggle(setIsNavVisible, isNavVisible);
+    }
+  };
+
   return (
-    <header className="flex flex-col md:flex-row md:items-center text-xl w-full">
-      <div className="flex justify-between py-4 w-full md:w-auto items-center">
+    <header className="flex flex-col py-4 md:flex-row md:items-center text-xl w-full">
+      <div className="flex justify-between w-full md:w-auto items-center">
         <h1 className="font-extrabold text-4xl text-blue-500">Codax</h1>
         <figure
           className="text-4xl text-blue-500 hover:text-blue-400 hover:cursor-pointer border-transparent border-4 rounded-lg hover:shadow-lg hover:border-4 hover:border-blue-400 hover:opacity-80 transition-all duration-300 md:hidden"
           onClick={() => handleToggle(setIsNavVisible, isNavVisible)}
+          onKeyDown={handleKeyPress}
+          tabIndex={0}
+          role="button"
+          aria-expanded={isNavVisible}
+          aria-label={isNavVisible ? "Close menu" : "Open menu"}
         >
           {isNavVisible ? <IoClose /> : <BiMenuAltRight />}
         </figure>
       </div>
-      <div
-        className={`w-full ${
-          isNavVisible ? "block" : "hidden"
-        } md:block`}
-      >
+      <div className={`w-full ${isNavVisible ? "block" : "hidden"} md:block`}>
         <NavContent />
       </div>
     </header>
--- src/components/header/navContent.jsx
+++ src/components/header/navContent.jsx
@@ -5,16 +5,22 @@ const NavContent = () => {
 
   return (
     <nav>
-      <ul className="flex flex-col *:self-start md:flex-row md:justify-end gap-4 lg:gap-8 text-blue-900">
+      <ul className="flex flex-col mt-8 md:mt-2 *:self-start md:*:self-center md:flex-row md:justify-end gap-6 lg:gap-8 text-blue-900">
         {listItems.map((item, index) => (
           <li
             key={index}
-            className="border-b-4 px-0 border-transparent hover:opacity-80  hover:ml-4 md:hover:ml-0 hover:border-blue-600 hover:cursor-pointer transition-all duration-300 p-2 "
+            className="border-b-4 px-0 py-2 border-transparent hover:opacity-80  hover:ml-4 md:hover:ml-0 hover:border-blue-600 hover:cursor-pointer transition-all duration-300"
           >
-            {item}
+            <a
+              href={`#${item.toLowerCase().replace(" ", "-")}`}
+              className="focus:outline-none focus:outline-offset-8 focus:outline-4 focus:outline-inherit"
+              tabIndex={0}
+            >
+              {item}
+            </a>
           </li>
         ))}
-        <Button>Purchase now</Button>
+        <Button className="text-white">Purchase now</Button>
       </ul>
     </nav>
   );
